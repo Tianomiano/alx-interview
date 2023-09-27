3763ee0f061eb4e4019aa208f6771da06292285d
Tianomiano <patomiano6@gmail.com>
2023-09-27 14:17:53 +0300

    pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,25 +1,22 @@
 #!/usr/bin/python3
-# an implementation of a function pascal_triangle(n) that generates
-#  Pascal's Triangle up to the n-th level and returns it as a list of lists of numbers
+"""Pascal Triangle Interview Challenge for Alx project"""
 
 
 def pascal_triangle(n):
-    # This function will generate Pascal's Triangle up to level n
+    """returns a list of lists of numbers
+    representing the pascal triangle"""
     if n <= 0:
         return []
 
-    # list is initialized with n zeros
     pascal_triangle = [0] * n
 
     for i in range(n):
-        # generate each row of Pascal's Triangle.
-        # Pascal's Triangle rows have one more element than their row number
+        # This code snipet define and explained that  a row is  fill first and last idx with 1
         row = [0] * (i+1)
         row[0] = 1
         row[len(row) - 1] = 1
 
         for j in range(1, i):
-            # calculate the middle elements of the current row
             if j > 0 and j < len(row):
                 x = pascal_triangle[i - 1][j]
                 y = pascal_triangle[i - 1][j - 1]
