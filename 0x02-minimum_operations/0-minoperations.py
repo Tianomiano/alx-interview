cdb492da7adb4b4cabc31b43cfb7da69e2967e72
Tianomiano <patomiano6@gmail.com>
2023-10-10 11:38:13 +0300

    minOperations calculates fewest number of operations needed to result in exactly n H characters in a file

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+minOperations function which calculates,
+the fewest number of operations needed,
+to result in exactly n 'H' characters in a file.
+"""
+
+
+def minOperations(n):
+    """
+    factors n and finds the largest factor,
+    that can be used to perform Paste operations,
+    continuously reducing n until it reaches 1. 
+    """
+    operations = 0
+    min_operations = 2
+    while n > 1:
+        while n % min_operations == 0:
+            operations += min_operations
+            n /= min_operations
+        min_operations += 1
+    return operations
